ff0db47d5f97303540baaebac65ccd3b474a3384
Jarek <z.jarek92@gmail.com>
2018-08-15 12:17:17 +0200

    Dispatching actions in fetch

--- src/state/fetchUsers.js
+++ src/state/fetchUsers.js
@@ -1,5 +1,23 @@
-const initialState = {}
+const SET_USERS = 'fetchUsers/SET_USERS' //nazwa akcji
+
+// action creator
+export const setUsersAction = data => ({
+    type: SET_USERS,
+    data
+})
+
+const initialState = {
+    users: null
+}
 
 export default (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case SET_USERS:
+            return {
+                ...state,
+                users: action.data
+            }
+        default:
+            return state
+    }
 }
